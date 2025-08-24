7fafb8c3eb5354a09d09363ff8306f04d2f427d2
Samun Chowdhury <samun315@gmail.com>
2025-08-24 15:07:21 +0600

    template integration for frontend, master and welcome page created

---
+++ resources/views/frontend/layout/slider.blade.php
@@ -0,0 +1,61 @@
+<!-- Home Slider -->
+        <div class="home-slider owl-theme owl-carousel">
+            <div class="slider-item slider-item-img">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="slider-text">
+                                <div class="slider-shape">
+                                    <img src="frontend/assets/img/home-one/home-slider/1.png" alt="Shape">
+                                </div>
+                                <h1>Exceptional Health Care for Woman</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                                <div class="common-btn">
+                                    <a href="appointment.html">Get Appointment</a>
+                                    <a class="cmn-btn-right" href="about.html">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider-item slider-item-img">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="slider-text">
+                                <div class="slider-shape-two">
+                                    <img src="frontend/assets/img/home-one/home-slider/2.png" alt="Shape">
+                                </div>
+                                <h1>Caring Health is Important Than All</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                                <div class="common-btn">
+                                    <a href="appointment.html">Get Appointment</a>
+                                    <a class="cmn-btn-right" href="about.html">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="slider-item slider-item-img">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="slider-text">
+                                <div class="slider-shape-three">
+                                    <img src="frontend/assets/img/home-one/home-slider/3.png" alt="Shape">
+                                </div>
+                                <h1>We Offer Highly Treatments</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                                <div class="common-btn">
+                                    <a href="appointment.html">Get Appointment</a>
+                                    <a class="cmn-btn-right" href="about.html">Learn More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Home Slider -->
