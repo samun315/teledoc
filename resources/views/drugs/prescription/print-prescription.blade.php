e5770bf5ba44fe493fe31c891ef6685379641ffb
Samun Chowdhury <samun315@gmail.com>
2026-04-29 19:03:45 +0600

    prescription logo path modified

--- resources/views/drugs/prescription/print-prescription.blade.php
+++ resources/views/drugs/prescription/print-prescription.blade.php
@@ -321,6 +321,8 @@
             @php
                 $contactWhatsapp = siteSetting('contact_whatsapp', siteSetting('contact_phone_1', ''));
                 $contactEmail = siteSetting('contact_email_1', '');
+                echo $contactWhatsapp;
+                echo $contactEmail;
             @endphp
             <div class="doctor-footer-info">
                 <b>Athful's-Teledoc Healthcare Platform.</b><br>
